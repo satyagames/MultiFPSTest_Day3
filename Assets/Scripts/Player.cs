54346514cddbfbd045a2a41cd1fb47abb48067ca
satyadev <satyagames@gmail.com>
2017-04-04 16:32:53 +0530

    files uploaded

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool>{}
+
+public class Player : NetworkBehaviour {
+
+	[SyncVar] public string playerName;
+	[SyncVar] public Color playerColor;
+
+	[SerializeField] ToggleEvent onToggleShared;
+	[SerializeField] ToggleEvent onToggleLocal;
+	[SerializeField] ToggleEvent onToggleRemote;
+	[SerializeField] float respawnTime = 5.0f;
+
+	static List<Player> players = new List<Player> ();
+
+	GameObject mainCamera;
+	//NetworkAnimator anim;
+	void Start()
+	{
+
+		OnColorChanged (playerColor);
+		OnNameChanged (playerName);
+		//anim = GetComponent<NetworkAnimator> ();
+		mainCamera = Camera.main.gameObject;
+
+		EnablePlayer ();
+	}
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+
+		//anim.animator.SetFloat ("Speed", Input.GetAxis ("Vertical"));
+		//anim.animator.SetFloat ("Strafe", Input.GetAxis ("Horizontal"));
+	}
+
+	[ServerCallback]
+	void OnEnable()
+	{
+		if (!players.Contains (this))
+			players.Add (this);
+	}
+
+	[ServerCallback]
+	void OnDisable()
+	{
+		if (players.Contains (this))
+			players.Remove (this);
+	}
+
+
+	void DisablePlayer()
+	{
+		if (isLocalPlayer) 
+		{
+			PlayerCanvas.canvas.HideReticule ();
+			mainCamera.SetActive (true);
+		}
+
+		onToggleShared.Invoke (false);
+
+		if (isLocalPlayer)
+			onToggleLocal.Invoke (false);
+		else
+			onToggleRemote.Invoke (false);
+	}
+
+	void EnablePlayer()
+	{
+		if (isLocalPlayer) 
+		{
+			PlayerCanvas.canvas.Initialize ();
+			mainCamera.SetActive (false);
+		}
+
+		onToggleShared.Invoke (true);
+
+		if (isLocalPlayer)
+			onToggleLocal.Invoke (true);
+		else
+			onToggleRemote.Invoke (true);
+	}
+
+	public void Die()
+	{
+		/*if(isLocalPlayer || playerControllerId == -1)
+				anim.SetTrigger ("Died");*/
+
+		if (isLocalPlayer) 
+		{
+			PlayerCanvas.canvas.WriteGameStatusText ("You Died !...");
+			PlayerCanvas.canvas.PlayDeathAudio ();
+
+		}
+
+		DisablePlayer ();
+
+		Invoke ("Respawn",respawnTime);
+	}
+	void Respawn()
+	{
+		/*if(isLocalPlayer || playerControllerId == -1)
+			anim.SetTrigger ("Restart");*/
+
+
+
+		if (isLocalPlayer) 
+		{
+			Transform spawn = NetworkManager.singleton.GetStartPosition ();
+			transform.position = spawn.position;
+			transform.rotation = spawn.rotation;
+
+		}
+		EnablePlayer ();
+	}
+
+	void OnNameChanged(string value)
+	{
+		playerName = value;
+		gameObject.name = playerName;
+		GetComponentInChildren<Text> (true).text = playerName;
+	}
+	void OnColorChanged(Color value)
+	{
+		playerColor = value;
+		GetComponentInChildren<RendererToggler> ().ChangeColor (playerColor);
+	}
+
+	[Server]
+	public void Won()
+	{
+		for (int i = 0; i < players.Count; i++)
+			players [i].RpcGameOver (netId, name);
+
+		Invoke ("BackToLobby", 5f);
+	}
+
+	[ClientRpc]
+	void RpcGameOver(NetworkInstanceId networkID, string name)
+	{
+		DisablePlayer ();
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+
+		if (isLocalPlayer)
+		{
+			if (netId == networkID)
+				PlayerCanvas.canvas.WriteGameStatusText ("You Won!");
+			else
+				PlayerCanvas.canvas.WriteGameStatusText ("Game Over!\n" + name + " Won");
+		}
+	}
+
+	void BackToLobby()
+	{
+		FindObjectOfType<NetworkLobbyManager> ().SendReturnToLobby ();
+	}
+
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShooting : NetworkBehaviour {
+
+	[SerializeField] float shotCoolDown = 0.3f;
+	[SerializeField] int killsToWin = 5;
+	[SerializeField] Transform firePosition;
+	[SerializeField] ShotEffectManager shotEffects;
+
+	[SyncVar (hook = "OnScoreChanged")] int score;
+
+	Player player;
+	float elaspedTime;
+	bool canShot;
+
+	void Start () 
+	{
+		player = GetComponent<Player> ();
+		shotEffects.Initialize ();
+
+		if (isLocalPlayer) 
+		{
+			canShot = true;
+		}
+	}
+
+	[ServerCallback]
+	void OnEnable()
+	{
+		score = 0;
+	}
+
+
+	void Update () 
+	{
+		if (!canShot)
+			return;
+
+		elaspedTime += Time.deltaTime;
+
+		if (Input.GetButtonDown ("Fire1") && elaspedTime > shotCoolDown) 
+		{
+			elaspedTime = 0f;
+			CmdFireShot (firePosition.position, firePosition.forward);
+		}
+
+	}
+	[Command]
+	void CmdFireShot(Vector3 origin, Vector3 direction)
+	{
+		RaycastHit hit;
+
+		Ray ray = new Ray (origin, direction);
+		Debug.DrawRay (ray.origin,ray.direction *3f,Color.red, 1f);
+
+		bool result = Physics.Raycast (ray, out hit, 50f); 	
+
+		if (result) 
+		{
+			//health stuff
+			PlayerHealth enemy = hit.transform.GetComponent<PlayerHealth> ();
+
+			if (enemy != null) 
+			{
+				bool wasKillShot = enemy.TakeDamage ();
+
+				if (wasKillShot && ++score >= killsToWin)
+					player.Won (); 
+
+			}
+		}
+		RpcProcessShotEffect (result, hit.point);
+	}
+
+	[ClientRpc]
+	void RpcProcessShotEffect( bool playImpact, Vector3 point)
+	{
+		shotEffects.PlayShotEffects ();
+
+		if (playImpact) 
+		{
+			shotEffects.PlayImpactEffects (point);
+		}
+	}
+
+	void OnScoreChanged(int value)
+	{
+		score = value;
+		if (isLocalPlayer)
+			PlayerCanvas.canvas.SetKills (value);
+	}
+
+	/*public void FireAsBot()
+	{
+		CmdFireShot (firePosition.position, firePosition.forward);
+	}*/
+}
